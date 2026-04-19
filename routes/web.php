b6ec2d57523777e3b4c6a1321d14c405d8e91429
robinschoenmaker <robin.schoenmaker@gereedschapbeheer.nl>
2026-04-19 20:29:45 +0200

    Add Nginx config for Nuxt proxy and update routes to reflect proxy setup

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
+/*
+ * GET / wordt door Nginx naar de Nuxt-server gedeeld (zie deploy/nginx-ploi-nuxt-proxy.conf).
+ * Legacy POST /analyze blijft voor oude clients; de app gebruikt POST /api/analyze.
+ */
 Route::post('/analyze', [ListingController::class, 'analyze']);
 
 Route::get('/report/{idOrSlug}/pdf', [ListingController::class, 'reportPdf'])
